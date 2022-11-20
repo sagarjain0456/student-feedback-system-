6c00c4f16b7e054bd541b2630503d170229d5561
Sagar Jain <sagarjain0456@gmail.com>
2022-11-20 17:19:54 +0530

    added signup and login page and completed the username and password verification for signup and login

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const db =process.env.DATABASE;
+const request = require("request");
+const encrypt = require("mongoose-encryption");
 
 mongoose.connect(db);
 // console.log(process.env);
@@ -24,6 +26,70 @@ app.get("/signup.html", function(req, res) {
 app.get("/login.html", function(req, res) {
   res.sendFile(__dirname + "/login.html");
 });
+
+
+const userSchema = new mongoose.Schema ({
+firstname:String,
+lastname: String,
+email: String,
+password: String
+});
+
+
+const secret =process.env.SECRET;
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
+
+const User = new mongoose.model("student-feedback-portal-collection", userSchema);
+
+
+
+// Starting of the Handling of post request for signup.html route
+
+app.post("/signup.html", function(req,res){
+ const newUser = new User({
+   email: req.body.email,
+   password: req.body.password,
+   firstname: req.body.firstname,
+   lastname: req.body.lastname
+ });
+
+
+
+ newUser.save(function(err){
+    if(err){
+      console.log(err);
+    }
+
+    else{
+      res.sendFile( __dirname + "/aftersignup.html");
+    }
+
+  });
+ })
+ // End of handling of the post request for signup.html route
+
+
+
+ app.post("/login.html", function(req,res){
+ const username = req.body.email;
+ const password = req.body.password;
+ //User is the name of the collection
+ User.findOne({email: username}, function(err, foundUser){
+   if(err){
+     console.log(err);
+   }
+
+   else{
+     if(foundUser){
+       if(foundUser.password === password){
+         res.sendFile(__dirname + "/aftersignup.html");
+       }
+     }
+   }
+
+ });
+
+ });
 
 
 
